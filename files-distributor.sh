d5701e2d338e8001bcae14f5006e23f5c901c2e8
gitmahin <nimahin25@gmail.com>
2025-07-04 13:47:54 +0600

    update

--- files-distributor.sh
+++ files-distributor.sh
@@ -1,7 +1,15 @@
-cd example-npm 
-mkdir src
-cd src
-mkdir tests
-cd ../../
-cp example-core/src/*.sh example-npm/src/
-cp example-core/src/tests/test.bats example-npm/src/tests/
+# !/bin/bash
+
+example_core="example-core"
+example_npm="example-npm"
+example_zenity="example-zenity"
+
+[[ -d "$example_core" ]] && mkdir -p "$example_core/src/tests"
+
+# cd example-npm 
+# mkdir src
+# mkdir src/tests
+# cd src
+# cd ../../
+# cp example-core/src/*.sh example-npm/src/
+# cp example-core/src/tests/test.bats example-npm/src/tests/
