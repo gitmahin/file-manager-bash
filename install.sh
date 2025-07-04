cfd4ceaf902497d61d7053f0a6c9f5a86bf18df7
gitmahin <nimahin25@gmail.com>
2025-07-04 08:59:50 +0600

    remove

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install git
-git submodule add --force  https://github.com/bats-core/bats-support.git src/tests/bats/test_helper/bats-support
-git submodule add --force  https://github.com/bats-core/bats-assert.git src/tests/bats/test_helper/bats-assert
+git submodule add --force  https://github.com/bats-core/bats-support.git src/tests/test_helper/bats-support
+git submodule add --force  https://github.com/bats-core/bats-assert.git src/tests/test_helper/bats-assert
 cd ~
 sudo rm -rf bats-core
 git clone https://github.com/bats-core/bats-core.git
