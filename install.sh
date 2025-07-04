fb2f83132d7f8a46ba6f3866029125dce91898ba
gitmahin <nimahin25@gmail.com>
2025-07-04 13:09:33 +0600

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 # !/bin/bash
-current_path=$(pwd)
 
-# sudo apt-get install git
-# cd ~
-# sudo rm -rf bats-core
-# git clone https://github.com/bats-core/bats-core.git
-# cd bats-core/
-# sudo ./install.sh /usr/local
-# which bats
-# bats --version
-
-# cd "$current_path"
 chmod -v 755 install.sh test.sh start.sh
 chmod -v 755 src/filemanager.sh
 source "./test.sh"
 pnpm i
- 
+pnpm test
+pnpm start
